77d189ce2e3ff41ffbd755f4d05ce0cc1557cffa
Murilo <murilomatias@alunos.utfpr.edu.br>
2020-12-13 19:17:28 -0300

    Installed sass loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require('sass'),
+      },
+    },
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
