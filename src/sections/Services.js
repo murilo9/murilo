26a1c461f6340a65fcb2e915d4aca1c51a06046e
Murilo <murilomatias@alunos.utfpr.edu.br>
2020-12-14 22:26:00 -0300

    Services section

--- src/sections/Services.js
+++ src/sections/Services.js
@@ -1,11 +1,53 @@
+/* eslint-disable max-len */
 import * as React from 'react';
-import {Container} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
+import '../styles/sections/Services.scss';
+
+/**
+ * icons: https://www.flaticon.com/authors/phatplus
+ */
 
 class ServicesSection extends React.Component {
   render() {
     return (
-      <section>
-        <Container>Services</Container>
+      <section id="services">
+        <Container>
+          <div className="d-flex justify-content-center">
+            <h2 className="green-title-border">Serviços</h2>
+          </div>
+          <Row className="mt-4">
+            <Col className="d-flex flex-column align-items-center">
+              <object type="image/svg+xml" data="computer.svg"
+                className="mh-flat-icon mb-3"></object>
+
+              <h3 className="mb-2">Aplicações Web</h3>
+              <p className="color-light-gray">
+                Construção de aplicações web modernas com Vue, React ou Angular
+                com backend composto de API REST ou de serviços baseados em nuvem
+                (Firebase, Heroku).
+              </p>
+            </Col>
+            <Col className="d-flex flex-column align-items-center">
+              <object type="image/svg+xml" data="worldwide.svg"
+                className="mh-flat-icon mb-3"></object>
+              <h3 className="mb-2">APIs REST</h3>
+              <p className="color-light-gray">
+                APIs feitas em Nodejs para prover serviços à sua aplicação de front-end
+                utilizando bancos de dados relacionais (MySQL) ou não-relacionais (MongoDB)
+                e seus respectivos ORMs (Sequelize, Mongoose).
+              </p>
+            </Col>
+            <Col className="d-flex flex-column align-items-center">
+              <object type="image/svg+xml" data="online.svg"
+                className="mh-flat-icon mb-3"></object>
+              <h3 className="mb-2">Landing Pages e Sites</h3>
+              <p className="color-light-gray">
+                Páginas feitas usando Nuxtjs visando a melhor performance e
+                bons resultados em motores de busca (SEO).
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </section>
     );
   }
