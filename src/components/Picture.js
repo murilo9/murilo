b8bc97f80c5b21255883b3bfc3739470362d04a6
Murilo <murilomatias@alunos.utfpr.edu.br>
2020-12-16 07:11:03 -0300

    Picture component refactoring

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -2,19 +2,43 @@
 import * as React from 'react';
 
 class Picture extends React.Component {
-  getStyle() {
+  getContainterStyle() {
+    return {
+      width: this.props.size,
+      height: this.props.size,
+      position: 'relative',
+    };
+  }
+
+  getSquareStyle() {
+    return {
+      border: `2px solid ${this.props.color === 'blue' ? '#5DEEFF' : '#66E782'}`,
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      left: this.props.left || '0',
+      right: this.props.right || '0',
+      top: this.props.top || '0',
+      bottom: this.props.bottom || '0',
+    };
+  }
+
+  getImgStyle() {
     return {
       backgroundImage: `url(${this.props.src})`,
       backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      width: this.props.size,
-      paddingTop: this.props.size,
+      width: '100%',
+      paddingTop: '100%',
+      position: 'relative',
     };
   }
 
   render() {
     return (
-      <div className="mh-picture" style={this.getStyle()} className={this.props.className}>
+      <div style={this.getContainterStyle()}
+        className={'mh-picture ' + this.props.className}>
+        <div style={this.getSquareStyle()}></div>
+        <div style={this.getImgStyle()} ></div>
       </div>
     );
   }
