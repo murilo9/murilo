c56392b1391037b168878c8ce4865bff42912bb3
Murilo <murilomatias@alunos.utfpr.edu.br>
2020-12-31 19:21:56 -0300

    Favicon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@ module.exports = {
         implementation: require('sass'),
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/favicon.png`,
+      },
+    },
     'gatsby-plugin-fontawesome-css',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
